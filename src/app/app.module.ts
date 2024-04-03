686feeed5b42a6fc5e1390ed4d65fe4fa8bf9e7e
chieh <chieh@25sprout.com>
2024-04-03 18:57:00 +0800

    feat: add PowerBoostCalculatorComponent, ExponentialStrengthPipe in declarations, and update package-lock.json.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PowerBoostCalculatorComponent } from "./power-boost-calculator.component";
+import { ExponentialStrengthPipe } from './exponential-strength.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
+    AppComponent,
+    PowerBoostCalculatorComponent,
+    ExponentialStrengthPipe,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule
+  ]
 })
 export class AppModule { }
