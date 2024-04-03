c75f17011a14dee7e0abe63ac95e30051d9af625
chieh <chieh@25sprout.com>
2024-04-03 16:22:43 +0800

    feat: add station-list.const.ts to present station list, and rewrite the app.component.html to finish the station list app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { stationList } from './station-list.const';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tour-of-heroes';
+  list = stationList;
 }
---
+++ src/app/station-list.const.ts
@@ -0,0 +1,25 @@
+export const stationList = [
+  {
+    station: "Kings Cross",
+    Destination: "Edinburgh",
+    UpdateTime: "2018-10-15T22:14:41.557",
+  }
+  ,
+  {
+    station: "Kings Cross2",
+    Destination: "Edinburgh2",
+    UpdateTime: "2018-10-15T22:14:41.557",
+  }
+  ,
+  {
+    station: "Kings Cross3",
+    Destination: "Edinburgh3",
+    UpdateTime: "2018-10-15T22:14:41.557",
+  }
+  ,
+  {
+    station: "Kings Cross4",
+    Destination: "Edinburgh4",
+    UpdateTime: "2018-10-15T22:14:41.557",
+  }
+];
